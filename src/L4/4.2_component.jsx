8a0a24e7b9a9c2c60386da351d8d60e1886e50e6
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-03-19 19:01:26 +0600

    4.2 React Compnent

---
+++ src/L4/4.2_component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class MyApp extends React.Component {
+
+    // Data / Logic Layer (Optional)
+    /**
+     * 1. State
+     * 2. Variable or properties
+     * 3. Function or Methods
+     * 4. Life Cycle Methods
+     */
+
+    render() {
+    
+    // Represtation Layer (Required)
+    /**
+     * 4. JSX (JS XML)
+     */
+
+        return <h1>4.2 Intro React Component</h1>;
+
+    }
+}
+
+export default MyApp;
