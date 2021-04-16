0a050835520ff00bc07a53c7ef24a9506204c222
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-04-16 11:19:53 +0600

    6.2 Which is the best method to Style CSS in React

---
+++ src/L6/6.2_style_css.jsx
@@ -0,0 +1,8 @@
+// There are four ways to Style CSS in React
+
+// 1. Inline CSS. [
+//     Which has some limitation. like Media query, hover state, Xudo Class etc can't use
+// ]
+// 2. CSS class. [Where CSS Scope is still exist]
+// 3. CSS Module [Best Method where CSS scope is not exist]
+// 4. JSS; Which is like to a framework. ex. Syled Component.
