8aef2e8a399b392b6d064964bf960c0147ac5a57
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-04-16 16:59:26 +0600

    7.2 What is Event

---
+++ src/L7/7.2_event.jsx
@@ -0,0 +1,11 @@
+/**
+ * What is Event?
+ * Anser: An event is an object which only triggers when something special occurs and return an object to
+ * the listener which contains information related to that event.
+ * Ex: Button click, input element, Mouse enter, mouse leave etc
+ * 
+ * N.B. Event is the only way to interact with users and get feedback from users.
+ * An event has
+ *  * Name
+ *  * Listener
+ */
