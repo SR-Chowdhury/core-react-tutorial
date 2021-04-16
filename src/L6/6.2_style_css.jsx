cd4892880153e1e827bc2bf0ca2a41f346fd4832
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-04-16 11:22:05 +0600

    6.2 4 Methods to Style CSS in React

--- src/L6/6.2_style_css.jsx
+++ src/L6/6.2_style_css.jsx
@@ -1,8 +1,10 @@
-// There are four ways to Style CSS in React
+/**
+ * There are four ways to Style CSS in React
 
-// 1. Inline CSS. [
-//     Which has some limitation. like Media query, hover state, Xudo Class etc can't use
-// ]
-// 2. CSS class. [Where CSS Scope is still exist]
-// 3. CSS Module [Best Method where CSS scope is not exist]
-// 4. JSS; Which is like to a framework. ex. Syled Component.
+    1. Inline CSS. [
+         Which has some limitation. like Media query, hover state, Xudo Class etc can't use
+        ]
+    2. CSS class. [Where CSS Scope is still exist]
+    3. CSS Module [Best Method where CSS scope is not exist]
+    4. JSS; Which is like to a framework. ex. Syled Component.
+ */
