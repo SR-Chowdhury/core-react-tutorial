508cf89c71da72f722b4e5b65c6205603866715e
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-04-18 22:01:34 +0600

    8.3 one changes in name

--- src/L8/8.3_BIO_radio_checkbox.jsx
+++ src/L8/8.3_BIO_radio_checkbox.jsx
@@ -8,19 +8,19 @@ class MyApp extends React.Component {
         bio: '',
         birthday: '',
         gender: '',
-        checkBox: false
+        agree: false
     }
     changeHandler = event => {
         this.setState({
             [event.target.name]: event.target.value
         });
     }
-    handleCheckbox = event => {
-        this.setState({checkBox: event.target.checked});
+    handleagree = event => {
+        this.setState({agree: event.target.checked});
     }
 
     render() {
-        const {name, country, birthday, bio, checkBox} = this.state;
+        const {name, country, birthday, bio, agree} = this.state;
         return (
             <div className='p-5'>
                 <h1 className='text-center'>Bismillahir Rahmanir Rahim</h1><hr/>
@@ -43,7 +43,7 @@ class MyApp extends React.Component {
                     <input className="m-2" type="radio" onChange={this.changeHandler} name='gender' value="Female"/> Female
                     <input className="m-2" type="radio" onChange={this.changeHandler} name='gender' value="Other"/> Other
                 </div>
-                <input onChange={this.handleCheckbox} type="checkbox" checked={checkBox} name="checkBox"/>&nbsp; I agree the all terms and conditions. <br/><br/>
+                <input onChange={this.handleagree} type="checkbox" checked={agree} name="agree"/>&nbsp; I agree the all terms and conditions. <br/><br/>
 
                 <button onClick ={() => console.log(this.state) }>Show date on Console</button>
                 
